6a85458fa67fef56d9537093243d5bdfafc239a4
ArnaudBvd <bravard.arnaud@gmail.com>
2023-06-24 22:40:24 +0200

    Mise en forme de la page 404

--- View/errors/404.php
+++ View/errors/404.php
@@ -1,23 +1,26 @@
 <?php
-// include 'View/parts/header.php';
+include 'View/parts/header.php';
 ?>
 <div class="container">
-    <h1 class="text-center">Oops ! Cette page n'existe pas</h1>
+    <h1 class="text-center my-5">Oops ! Cette page n'existe pas</h1>
 
     <?php
     if ($_GET["scope"] == 'moto') {
-        echo ('<h2 class="text-center text-light mt-4">Cette moto a probablement été supprimée</h2>');
+        echo ('<h2 class="text-center mt-4">Cette moto a probablement été supprimé...</h2>');
     }
     ?>
 
-    <div class="d-flex justify-content-center mt-5">
+    <div class="d-flex justify-content-center my-5">
         <a onclick="window.history.back()">
             <button>Retour</button>
         </a>
     </div>
 
-    <div class="my-4 d-flex justify-content-center">
-        <img src="public/img/debris.jpg" alt="debris" class="rounded">
+    <div class="my-5 d-flex justify-content-center">
+        <img src="public/icons/motor.png" alt="moteur de moto" class="rounded" width="350" height="400">
+    </div>
+    <div class="my-5 d-flex justify-content-center">
+        <img src="public/icons/oil.png" alt="tache d'huile" class="rounded" width="300" height="100">
     </div>
 
 </div>
