b34e2379759e20fa9964a0c8f9627555a4ae7046
ArnaudBvd <bravard.arnaud@gmail.com>
2023-06-24 22:25:29 +0200

    Mise en forme de la page form-add.php

--- View/motos/form-add.php
+++ View/motos/form-add.php
@@ -2,21 +2,25 @@
 include('View/parts/header.php');
 ?>
 
+<section id="section-add"></section>
+
 <div class="container mb-5">
-    <h1 class="text-center mb-4">Ajouter une moto</h1>
+    <h1 class="text-center my-5">Ajouter une moto</h1>
+
+    <p class="text-center fs-4 mb-5">Parce qu'on ne sait jamais quand une collection se finit, faites-vous donc plaisir en ajoutant des motos !</p>
 
     <div id="return-btn">
-        <a href="index.php?controller=moto&action=list" class="text-decoration-none">Retour</a>
+        <a href="index.php?controller=moto&action=list" class="text-decoration-none fs-5" id="btn-link">Retour</a>
     </div>
 
     <!-- <div class="my-4 d-flex justify-content-center">
         <img src="public/img/panorama.webp" alt="r2d2 projecte un hologramme" class="rounded" width="1180" height="445">
     </div> -->
 
-    <form method="post" enctype="multipart/form-data" class="row mt-4">
+    <form method="post" enctype="multipart/form-data" class="row my-5">
 
-        <div class="col-md-12 mb-3">
-            <label for="marque" class="form-label">Marque :</label>
+        <div class="col-md-12 mb-4">
+            <label for="marque" class="form-label fs-5">Marque :</label>
             <input type="text" value="<?php if (array_key_exists('marque', $_POST)) {
                                             echo ($_POST['marque']);
                                         } ?>" name="marque" id="marque" class="form-control
@@ -30,8 +34,8 @@ include('View/parts/header.php');
             </div>
         </div>
 
-        <div class="col-md-12 mb-3">
-            <label for="modele" class="form-label">Modele :</label>
+        <div class="col-md-12 mb-4">
+            <label for="modele" class="form-label fs-5">Modèle :</label>
             <input type="text" value="<?php if (array_key_exists('modele', $_POST)) {
                                             echo ($_POST['modele']);
                                         } ?>" name="modele" id="modele" class="form-control
@@ -45,8 +49,8 @@ include('View/parts/header.php');
             </div>
         </div>
 
-        <div class="col-md-12 mb-3">
-            <label for="validationCustom04" class="form-label">Type :</label>
+        <div class="col-md-12 mb-4">
+            <label for="validationCustom04" class="form-label fs-5">Type :</label>
             <select class="form-select
                  <?php if(array_key_exists("type", $errors)){echo('is-invalid');}?>" name="type" id="validationCustom04">
                 <option  value="">Pas d'infos</option>
@@ -65,8 +69,8 @@ include('View/parts/header.php');
             </div>
         </div>
 
-        <div class="col-md-12 mb-3">
-            <label for="image" class="form-label">Photo :</label>
+        <div class="col-md-12 mb-4">
+            <label for="image" class="form-label fs-5">Photo :</label>
             <input type="file" name="image" id="image" class="form-control
             <?php if (array_key_exists("image", $errors)) {echo ('is-invalid');} ?>">
             <div class="invalid-feedback">
@@ -77,10 +81,12 @@ include('View/parts/header.php');
         </div>
 
         <div class="d-flex justify-content-center mt-2">
-            <input type="submit" value="VALIDER" id="btn-validate">
+            <input type="submit" value="Valider" id="btn-validate">
         </div>
     </form>
 </div>
+
+<section class="mt-5" id="endpage-section"></section>
 
 <?php
 include('View/parts/footer.php');
