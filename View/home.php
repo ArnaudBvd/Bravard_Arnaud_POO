9865f991a797d785f089265b62c01cb62ec228fc
ArnaudBvd <bravard.arnaud@gmail.com>
2023-06-24 14:23:02 +0200

    Mise en forme de la navbar

--- View/home.php
+++ View/home.php
@@ -1,23 +1,22 @@
 <?php
 include('View/parts/header.php');
+include 'View/parts/slider.php';
+
 ?>
 
 <div class="container">
-    <h1 class="text-center mb-5">Bienvenue chez Moto Vroum Vroum !</h1>
 
-    <div class="my-4 d-flex justify-content-center">
-        <img src="public/img/duel.jpg" alt="deux sabres lasers se croisent" class="rounded" width="860" height="440">
-    </div>
+    <h1 class="text-center py-5">Bienvenue chez Moto Passion</h1>
 
     <div class="row">
         <div class="col-3"></div>
-        
+
         <div class="col-6  d-flex justify-content-center my-5">
             <a href="index.php?controller=moto&action=list" class="text-decoration-none text-center">
                 <button>Voir les motos</button>
             </a>
         </div>
-               
+
         <div class="col-3"></div>
 
     </div>
--- View/parts/navbar.php
+++ View/parts/navbar.php
@@ -1,24 +1,24 @@
 <header>
-    <nav class="navbar navbar-expand-lg mb-5 py-4">
+    <nav class="navbar navbar-expand-lg py-4">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#"><img src="public/img/logosw.png" alt="" width="100" height="50"></a>
+            <a class="navbar-brand" href="index.php"><img src="public/icons/motorcycle.png" alt="" width="100" height="100"></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav col-10">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="index.php">Accueil</a>
+                        <a class="nav-link fs-5" aria-current="page" href="index.php">Accueil</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="index.php?controller=moto&action=list">Gallerie</a>
+                        <a class="nav-link fs-5" href="index.php?controller=moto&action=list">Gallerie</a>
                     </li>                    
 
                     <li class="nav-item ms-5">
                         <?php if ($this->connexion_status() == false) {
-                            echo '<a class=" btn btn-success nav-link fw-semibold text-light p-2" href="index.php?controller=security&action=login">Connexion</a>';
+                            echo '<a class=" btn btn-success fs-5 nav-link fw-semibold px-3 p-2" href="index.php?controller=security&action=login">Connexion</a>';
                         } else if ($this->connexion_status() == true) {
-                            echo '<a class=" btn btn-danger nav-link fw-semibold text-light p-2" href="index.php?controller=security&action=logout">Déconnexion</a>';
+                            echo '<a class=" btn btn-danger fs-5 nav-link fw-semibold px-3 p-2" href="index.php?controller=security&action=logout">Déconnexion</a>';
                         }
                         ?>
                     </li>
@@ -28,7 +28,7 @@
                 <div class=" d-flex justify-content-center col-2">
                         <?php
                         if ($this->currentUser) {
-                            echo ('<a class="nav-link text-center text-warning float-end"> Bienvenue ' . $this->currentUser->getName(). '</a>');
+                            echo ('<a class="nav-link text-center fs-5 text-warning float-end"> Bienvenue ' . $this->currentUser->getName(). '</a>');
                         }
                         ?>
                 </div>
